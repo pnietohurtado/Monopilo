8852c5b3cabda4995fe68c56dcbc28f06d08979f
pablo <pablo@DESKTOP-SMQRCRD>
2025-01-21 16:07:47 +0100

    Inicio

---
+++ src/main/java/com/mycompany/monopoly/Monopoly.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.monopoly;
+
+/**
+ *
+ * @author pablo
+ */
+public class Monopoly {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
